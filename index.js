95327c2b9881ee02a518030edf8071f42a76fca8
leah forrest <leahforrest14@gmail.com>
2023-09-29 19:01:00 -0600

    create product tiles, reinforce format for Friday presentation and add nonfunctional js file for funsies

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log('this is testing out javascript but not doing much but logging...');
+
+document.title = 'this title is my first time changing a title with js';
+
+let test = 'This is me testing alerts in JavaScript';
+
+alert(test);
